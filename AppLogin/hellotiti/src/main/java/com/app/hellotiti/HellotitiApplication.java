a5d3b3057d7b4c57d6d04e815b228ccda1bef1e3
hieupnh12 <hieupnh12@gmail.com>
2025-05-14 16:05:24 +0700

    first commit

---
+++ AppLogin/hellotiti/src/main/java/com/app/hellotiti/HellotitiApplication.java
@@ -0,0 +1,13 @@
+package com.app.hellotiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellotitiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellotitiApplication.class, args);
+	}
+
+}
